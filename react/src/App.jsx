96659dbf33d3f0a96e96b40c4a0ca1f8501e2395
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-26 18:03:00 +0900

    react component practice fin

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
