cc3d1eaed60fc77916517d2e21a929ae4e9135b7
stanstanstanlee <stanstanstanlee@gmail.com>
2025-09-30 17:27:19 +0900

    numberState

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,13 +3,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import ProductContainer from "./components/ProductCard/ProductContainer";
+import StringState from "./components/State/StringState";
 ProductContainer;
 export default function App() {
   return (
     <div>
       {/* <Header /> */}
-      <Main />
+      {/* <Main /> */}
       {/* <Footer /> */}
+      <StringState />
     </div>
   );
 }
