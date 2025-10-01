5c6b39a4e09681dadbee7b99ed9e31c451b35508
stanstanstanlee <stanstanstanlee@gmail.com>
2025-10-01 13:44:09 +0900

    state4

--- react/src/components/Signup/Form.jsx
+++ react/src/components/Signup/Form.jsx
@@ -5,10 +5,19 @@ export default function Form() {
     email: "",
     password: "",
     confirmPassword: "",
+    level: "",
   });
+
+  const levels = [
+    { id: 1, value: "신입" },
+    { id: 2, value: "주니어" },
+    { id: 3, value: "시니어" },
+  ];
+
   function handleChange(event) {
-    const target = event["target"];
-    const { name, value } = target;
+    // const target = event["target"];
+    // const { name, value } = target;
+    const { name, value } = event["target"];
 
     const newForm = { ...form, [name]: value };
     setForm(newForm);
@@ -33,47 +42,74 @@ export default function Form() {
   return (
     <div>
       <form className="form" onSubmit={handleSubmit}>
-        <label>이메일</label> <br></br>
-        <input
-          className="border-2"
-          type="email"
-          name="email"
-          value={form["email"]}
-          onChange={(event) => {
-            handleChange(event);
-          }}
-        />
+        <label>
+          이메일
+          <br />
+          <input
+            className="border-2"
+            type="email"
+            name="email"
+            value={form["email"]}
+            onChange={(event) => {
+              handleChange(event);
+            }}
+          />
+        </label>
         <br></br>
-        <label>비밀번호</label> <br></br>
-        <input
-          className="border-2"
-          type="password"
-          name="password"
-          value={form["password"]}
-          onChange={(event) => {
-            handleChange(event);
-          }}
-          required
-        />
+        <label>
+          비밀번호 <br />
+          <input
+            className="border-2"
+            type="password"
+            name="password"
+            value={form["password"]}
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            required
+          />
+        </label>
         <br></br>
-        <label>비밀번호 확인</label> <br></br>
-        <input
-          className="border-2"
-          type="password"
-          name="confirmPassword"
-          value={form["confirmPassword"]}
-          onChange={(event) => {
-            handleChange(event);
-          }}
-          required
-        />
-        <br></br>
+        <label>
+          비밀번호 확인 <br />
+          <input
+            className="border-2"
+            type="password"
+            name="confirmPassword"
+            value={form["confirmPassword"]}
+            onChange={(event) => {
+              handleChange(event);
+            }}
+            required
+          />
+        </label>
+        <br />
         <label>레벨</label>
-        <br></br>
-        <input type="radio" name="level" value="신입" /> 신입
-        <input type="radio" name="level" value="주니어" /> 주니어
-        <input type="radio" name="level" value="시니어" /> 시니어
-        <br></br>
+        <br />
+        {levels.map((level) => {
+          // <label key={level.id}>
+          //   <input
+          //     type="radio"
+          //     name="level"
+          //     value={level.value}
+          //     checked={form.level === level.value}
+          //     onChange={handleChange}
+          //   />
+          // </label>;
+          return (
+            <label key={level.id}>
+              <input
+                type="radio"
+                name="level"
+                value={level.value}
+                checked={form.level === level.value}
+                onChange={handleChange}
+              />
+              {level.value}
+            </label>
+          );
+        })}
+        <br />
         <input
           className="bg-blue-500 text-white px-5 py-2 cursor-pointer"
           type="submit"
