007c579e79487165eaf8698fbb9ebedfdb276491
stanstanstanlee <stanstanstanlee@gmail.com>
2025-10-13 13:32:11 +0900

    router

--- react-router/src/pages/Home.jsx
+++ react-router/src/pages/Home.jsx
@@ -10,8 +10,9 @@ export default function Home() {
       {/* Link 컴포넌트 사용 */}
       <div style={{ margin: "20px 0" }}>
         <h3>Link 컴포넌트로 이동:</h3>
-        <Link to="/dummy/products">상품 목록</Link>
+        <Link to="/dummy/products">상품 목록</Link> <br />
         <Link to="/dummy/carts">장바구니</Link>
+        <br />
         <Link to="/dummy/posts">게시글</Link>
       </div>
 
@@ -19,7 +20,9 @@ export default function Home() {
       <div style={{ margin: "20px 0" }}>
         <h3>useNavigate 훅으로 이동:</h3>
         <button onClick={() => navigate("/dummy/products")}>상품 목록</button>
+        <br />
         <button onClick={() => navigate("/dummy/carts")}>장바구니</button>
+        <br />
         <button onClick={() => navigate("/dummy/posts")}>게시글</button>
       </div>
     </div>
